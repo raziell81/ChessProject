338a844ee9d19395f70c3f1419a8a45b7b609383
Andre <andre.alyam@outlook.com>
2020-05-08 11:38:39 -0300

    Criando a classe Posicao

--- ChessProject/Program.cs
+++ ChessProject/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace ChessProject
+namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
     }
 }
