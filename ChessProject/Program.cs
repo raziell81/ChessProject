4505ace3280bd6ef13401147b7df1371e7a82339
Andre <andre.alyam@outlook.com>
2020-05-09 12:23:08 -0300

    Melhorando a implementacao do metodo imprimirTabuleiro

--- ChessProject/Program.cs
+++ ChessProject/Program.cs
@@ -13,7 +13,9 @@ namespace xadrez_console
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 1));
+
+                tab.colocarPeca(new Peao(tab, Cor.Branca), new Posicao(3, 5));
 
                 Tela.imprimirTabuleiro(tab);
             }
