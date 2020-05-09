0e07df51620464cc80eb29fd40fb128ae4afdcff
Andre <andre.alyam@outlook.com>
2020-05-09 12:00:16 -0300

    Tratando erros com excecao personalizada

--- ChessProject/Program.cs
+++ ChessProject/Program.cs
@@ -8,11 +8,19 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            
-            Tela.imprimirTabuleiro(tab);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
+
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
         }
     }
