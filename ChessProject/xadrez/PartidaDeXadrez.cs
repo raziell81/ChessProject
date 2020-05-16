19023f0fd66f4baa899a4f72864c049f0ad130de
Andre <andre.alyam@outlook.com>
2020-05-16 09:24:18 -0300

    Correção: Mudando o nome do método para movimentoPossivel

--- ChessProject/xadrez/PartidaDeXadrez.cs
+++ ChessProject/xadrez/PartidaDeXadrez.cs
@@ -94,7 +94,7 @@ namespace xadrez
         }
         public void validarPosicaoDeDestino(Posicao origem, Posicao destino)
         {
-            if (!tab.peca(origem).podeMoverPara(destino))
+            if (!tab.peca(origem).movimentoPossivel(destino))
             {
                 throw new TabuleiroException("Destino escolhido inválido!");
             }
@@ -192,10 +192,11 @@ namespace xadrez
                     {
                         if (mat[i, j])
                         {
+                            Posicao origem = x.posicao;
                             Posicao destino = new Posicao(i, j);
-                            Peca pecaCapturada = executarMovimento(x.posicao, destino);
+                            Peca pecaCapturada = executarMovimento(origem, destino);
                             bool testeXeque = estaEmXeque(cor);
-                            desfazMovimento(x.posicao, destino, pecaCapturada);
+                            desfazMovimento(origem, destino, pecaCapturada);
                             if (!testeXeque)
                             {
                                 return false;
